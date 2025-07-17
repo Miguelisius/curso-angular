c0b411be2d48758adf605c83e0b79ebcffb8c2be
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-17 20:24:56 +0200

    Avances en Angular

--- bases/src/app/app.routes.ts
+++ bases/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {HeroPageComponent} from './pages/hero/hero-page.component';
 import { CounterPageComponent } from './pages/counter/counter-page.component';
-
+import { Dragonball } from './pages/dragonball/dragonball';
 export const routes: Routes = [
   {
     path: '',
@@ -10,6 +10,10 @@ export const routes: Routes = [
   {
     path: 'hero',
     component: HeroPageComponent,
+  },
+  {
+    path: 'dragonball',
+    component: Dragonball,
   },
   {
     path:'**',
---
+++ bases/src/app/pages/dragonball/dragonball.ts
@@ -0,0 +1,50 @@
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
+
+interface Character{
+  id:number;
+  name: string;
+  power: number;
+}
+
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [NgClass],
+  templateUrl: './dragonball.html',
+})
+export class Dragonball {
+
+  name = signal('Gohan')
+  power = signal(100)
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power:9001},
+    { id: 2, name: 'Vegeta', power:8000},
+    { id: 3, name: 'Piccolo', power:3000},
+    { id: 3, name: 'Yamcha', power:500},
+  ]);
+  /*powerClasses = computed(()=>{
+    return{
+      'text-danger': true,
+    }
+  })*/
+  addCharacter(){
+    if(!this.name() || !this.power() || this.power() <= 0){
+      return;
+    }
+    const newCharacter: Character={
+      id: this.characters().length+1,
+      name: this.name(),
+      power: this.power(),
+    };
+    this.characters.update(
+      (list)=>[... list, newCharacter]
+    );
+    this.resetFields();
+  }
+  resetFields(){
+    this.name.set('');
+    this.power.set(0);
+  }
+}
