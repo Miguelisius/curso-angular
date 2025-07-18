f62363fe65f36613d27aec720e3ce1e784e364f6
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-18 19:29:08 +0200

    servicios y componentes

--- bases/src/app/pages/dragonball-super/dragonball-super.ts
+++ bases/src/app/pages/dragonball-super/dragonball-super.ts
@@ -1,6 +1,7 @@
 import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
+import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
 
 interface Character{
   id:number,
@@ -12,12 +13,10 @@ interface Character{
 @Component({
   selector: 'dragonball-super',
   templateUrl: './dragonball-super.html',
-  imports: [CharacterListComponent],
+  imports: [CharacterListComponent, CharacterAddComponent],
 })
 export class DragonballSuper {
 
-  name = signal('')
-  power = signal(0)
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power:9001},
@@ -25,22 +24,10 @@ export class DragonballSuper {
 
   ]);
 
-  addCharacter(){
-    if(!this.name() || !this.power() || this.power() <= 0){
-      return;
-    }
-    const newCharacter: Character={
-      id: this.characters().length+1,
-      name: this.name(),
-      power: this.power(),
-    };
+  addCharacter(character: Character){
     this.characters.update(
-      (list)=>[... list, newCharacter]
+      (list) => [... list, character]
     );
-    this.resetFields();
   }
-  resetFields(){
-    this.name.set('');
-    this.power.set(0);
-  }
+
 }
