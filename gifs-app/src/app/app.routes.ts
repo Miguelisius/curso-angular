fe5c169f387255228192bd11cb3daa49ed55b8a1
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-19 18:50:51 +0200

    Nuevas Rutas search y trending y componentes

--- gifs-app/src/app/app.routes.ts
+++ gifs-app/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+  {
+    path: 'trending',
+    loadComponent: () => import('./gifs/pages/trending/trending.component'),
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./gifs/pages/search-pages/search-pages.component'),
+  },
+  {
+    path:'**',
+    redirectTo:'dashboard',
+  },
+
+
+
+];
---
+++ gifs-app/src/app/gifs/pages/search-pages/search-pages.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-pages',
+  imports: [],
+  templateUrl: './search-pages.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class SearchPagesComponent { }
