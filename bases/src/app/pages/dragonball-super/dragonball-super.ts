d9f27ad6a90e7f8109f1758ca125d03f4b2d446e
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-19 12:22:14 +0200

    efecttos en el servicio

--- bases/src/app/pages/dragonball-super/dragonball-super.ts
+++ bases/src/app/pages/dragonball-super/dragonball-super.ts
@@ -1,7 +1,8 @@
 import { NgClass } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { CharacterListComponent } from '../../components/dragonball/character-list/character-list.component';
 import { CharacterAddComponent } from '../../components/dragonball/character-add/character-add.component';
+import { DragonballService } from '../../services/dragonball.service';
 
 interface Character{
   id:number,
@@ -17,17 +18,8 @@ interface Character{
 })
 export class DragonballSuper {
 
+  public dragonballService = inject(DragonballService);
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power:9001},
-    { id: 2, name: 'Vegeta', power:8000},
 
-  ]);
-
-  addCharacter(character: Character){
-    this.characters.update(
-      (list) => [... list, character]
-    );
-  }
 
 }
