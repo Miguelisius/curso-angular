122795e66f3febff2af83a589789afc4f5ff1ae5
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-20 17:46:22 +0200

    crear peticion HTTP

--- gifs-app/src/environments/environment.ts
+++ gifs-app/src/environments/environment.ts
@@ -5,4 +5,5 @@ export const environment = {
   companyName2: 'App',
   companySlogan: 'Configura tus gifs',
   giphyApiKey: 'C6PzdHyYC3KaJ751uSDr5zzMc5BOWHON',
+  giphyUrl:'https://api.giphy.com/v1',
 };
