68372bc38b162f92f22d174b130326e85cf5e845
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-20 12:18:25 +0200

    menu dashboard

--- gifs-app/src/app/gifs/components/side-menun-options/side-menun-options.component.ts
+++ gifs-app/src/app/gifs/components/side-menun-options/side-menun-options.component.ts
@@ -1,8 +1,33 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+interface MenuOptions{
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menun-options',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './side-menun-options.component.html',
 })
-export class SideMenunOptionsComponent { }
+export class SideMenunOptionsComponent {
+
+  menuOptions: MenuOptions [] =[
+    {
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route:'/dashboard/trending',
+      icon:'fa-solid fa-chart-line',
+    },
+    {
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route:'/dashboard/search',
+      icon:'fa-solid fa-magnifying-glass',
+    },
+  ]
+
+}
