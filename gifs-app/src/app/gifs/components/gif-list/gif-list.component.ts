89b251d30b80adec2a14bb990b5e8726d8a9bbcc
Miguelisius <miguelgonzalezlopezz@gmail.com>
2025-07-20 13:16:10 +0200

    Actualizacion pg gif fin tema 6

--- gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
+++ gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
@@ -1,9 +1,11 @@
 import { GifListItemComponent } from './gif-list-item/gif-list-item.component';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list',
   imports: [GifListItemComponent],
   templateUrl: './gif-list.component.html',
 })
-export class GifListComponent { }
+export class GifListComponent {
+  gifs = input.required<string[]>();
+}
